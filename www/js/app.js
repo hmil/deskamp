10393a09e7940416aeb528118de095569cb8c5d7
Manu <chimyx@live.fr>
2013-04-27 22:46:59 +0200

    Just somme indent

--- www/js/app.js
+++ www/js/app.js
@@ -11,51 +11,52 @@
 
 
 define([
-    'views/widgetBar',
+    'views/WidgetBar',
     'backbone', 
-    'Router'],
-    function(WidgetBar){
+    'Router'
+    ],
+    function(WidgetBar,Backbone){
 
-    // App is a singleton object
-    var App = {};
-    
-    /*
-        This method is called by the loader when the dom is ready.
-    */
-    App.init = function () {
-        // Creates the router
-        this.router = new (require('Router'));
-        
-        if(!Backbone.history.start()){
-            window.location.hash = '/';
-        }
-        
-        var arguments = {};
-        
-        this.widgetBar = new WidgetBar({
-            el: '#widget_bar',
-            
-            widgets: [
-                
-                {
-                    name: "todo",
-                    title: "Todo list",
-                    icon: "todo/icon.png"
-                }, 
-                
-                {
-                    name: "sticky",
-                    title: "Sticky note",
-                    icon: "sticky/icon.png"
-                }
-            
-            ]
-        });
-        
-        
-        console.log("app initialized");
-    };
-    
-    
-    return window.app = App;
+        // App is a singleton object
+        var App = {};
+
+        /*
+            This method is called by the loader when the dom is ready.
+        */
+        App.init = function () {
+            // Creates the router
+            this.router = new (require('Router'));
+
+            if(!Backbone.history.start()){
+                window.location.hash = '/';
+            }
+
+            var arguments = {};
+
+            this.widgetBar = new WidgetBar({
+                el: '#widget_bar',
+
+                widgets: [
+
+                    {
+                        name: "todo",
+                        title: "Todo list",
+                        icon: "todo/icon.png"
+                    },
+
+                    {
+                        name: "sticky",
+                        title: "Sticky note",
+                        icon: "sticky/icon.png"
+                    }
+
+                ]
+            });
+
+
+            console.log("app initialized");
+        };
+
+
+        return window.app = App;
 });
