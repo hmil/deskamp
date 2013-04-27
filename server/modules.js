88f48fd8174f3fc3d99c5a331a3f3578c9ebfa00
Hadrien Milano <hadrien.milano@gmail.com>
2013-04-28 01:39:56 +0200

    fixed wrong modules detection (thx mac)

--- server/modules.js
+++ server/modules.js
@@ -17,12 +17,64 @@ module.exports = function(app){
     
     console.log("Please wait a few seconds for modules to load...");
     
+    function onViewFetched(){
+        // Counts fetched views
+        fetchedViews++;
+        
+        // When all views are fetched
+        if(fetchedViews == nbModules){
+            jsFile = 'define(["';
+            
+            for(var i in modules){
+                jsFile += modules[i].view;
+                if(i < modules.length -1)
+                    jsFile += '","';
+            }
+            jsFile += '"], function(';
+            
+            for(var i in modules){
+                jsFile += modules[i].name+'View';
+                if(i < modules.length -1)
+                    jsFile += ',';
+            }
+            jsFile += '){';
+            
+            jsFile += 'return {'
+            
+            for(var i in modules){
+                jsFile += modules[i].name+': { '
+                    + 'view: '+modules[i].name+'View,'
+                    + 'icon: "'+modules[i].icon+'",'
+                    + 'title: "'+modules[i].title+'",'
+                    + 'name: "'+modules[i].name+'"'
+                    +'}';
+                    
+                if(i < modules.length -1)
+                    jsFile += ',';
+            }
+            
+            jsFile += '}; });';
+            
+            console.log("modules js ready");
+        }
+    }
+    
+    function onCssFetched(){
+        fetchedStyles ++;
+                if(fetchedStyles == nbModules)
+                    console.log("All styles fetched");
+    }
+    
     /* Fetches an individual module */
     function fetchModule(name){
-    
+        if(/\..*/.exec(name)){
+            onViewFetched();
+            onCssFetched();
+            return;
+        }
+            
         fs.readFile(modulePath+name+'/package.json', function (err, data) {
             if (err) throw err;
-            
             var parsedPackage = JSON.parse(data);
             
             /* loading css */
@@ -31,9 +83,7 @@ module.exports = function(app){
                 
                 css += data;
                 
-                fetchedStyles ++;
-                if(fetchedStyles == nbModules)
-                    console.log("All styles fetched");
+                onCssFetched();
             });
             
             modules.push({
@@ -43,46 +93,7 @@ module.exports = function(app){
                 title: parsedPackage.title
             });
             
-            // Counts fetched views
-            fetchedViews++;
-            
-            // When all views are fetched
-            if(fetchedViews == nbModules){
-                jsFile = 'define(["';
-                
-                for(var i in modules){
-                    jsFile += modules[i].view;
-                    if(i < modules.length -1)
-                        jsFile += '","';
-                }
-                jsFile += '"], function(';
-                
-                for(var i in modules){
-                    jsFile += modules[i].name+'View';
-                    if(i < modules.length -1)
-                        jsFile += ',';
-                }
-                jsFile += '){';
-                
-                jsFile += 'return {'
-                
-                for(var i in modules){
-                    jsFile += modules[i].name+': { '
-                        + 'view: '+modules[i].name+'View,'
-                        + 'icon: "'+modules[i].icon+'",'
-                        + 'title: "'+modules[i].title+'",'
-                        + 'name: "'+modules[i].name+'"'
-                        +'}';
-                        
-                    if(i < modules.length -1)
-                        jsFile += ',';
-                }
-                
-                jsFile += '}; });';
-                
-                console.log("modules js ready");
-            }
-            
+            onViewFetched();
         });
         
        
