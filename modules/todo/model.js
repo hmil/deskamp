498dced85bdc29452e0f93f35171540cbfe324e7
Christophe Tafani-Dereeper <christophe.tafani-dereeper@epfl.ch>
2013-04-28 13:15:19 +0200

    Panell & modl

--- modules/todo/model.js
+++ modules/todo/model.js
@@ -12,18 +12,20 @@
         },
 
         addItem: function(item) {
-        	this.get('items').push(item);
+        	this.set('items', this.get('items').concat([item]));
         }, 
         checkItem: function(itemName) {
         	console.log("Checking "+itemName);
         	 _.find(this.get('items'), function(i) {
         		return itemName == i.name;
         	}).done = true;
+             this.trigger('change');
         }, 
         uncheckItem: function(itemName) {
         	 _.find(this.get('items'), function(i) {
         		return itemName == i.name;
         	}).done = false;
+             this.trigger('change');
         }, 
         removeItem: function(itemName) {
         	this.set('items', _.filter(this.get('items'), function(item) {
