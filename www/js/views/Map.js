4bf2fa94b64f8a3d61e326fc0b8d3a9f8d2f98ac
Philippe Schwaller <SchwallerP@s0-0.paconsult7.bbnplanet.net>
2013-04-28 07:50:32 +0200

    Map

--- www/js/views/Map.js
+++ www/js/views/Map.js
@@ -5,46 +5,53 @@ define([
     'jqueryUI'], function(templateString){
     
     return Backbone.View.extend({
+
         
         initialize: function(){
             
             this.template = _.template( templateString );
             
             this.render();
+
+
+        },
+
+        newpos: function(ri,to) {
+            $('#target').css({'top': "'"+to +"%'", 'right': "'"+ ri +"%'"});
         },
         
         render: function(){
+
+
+
             this.$el.html( this.template() );
 
-                
+            _this = this;
+            
+            _this.draggable = this.$('#target');
+            initialpos ={};
 
-
-                console.log($('#target').offsetLeft);
-
-
-
-
+            initialpos = _this.draggable.offset();
 
             this.$('[data-draggable="draggable"]').draggable({
                 containment: "#map",
 
-                drag: function(event, ui){
 
-                    var x = document.getElementById('target').offsetLeft;
-                    var y = document.getElementById('target').offsetTop;
+                drag: function(event, ui){    
+                        console.log(initialpos);             
 
-                    var viewportWidth = $(window).width();
-                    var viewportHeight = $(window).height();
+                        currentpos = _this.draggable.offset();
 
-                    var vScrollPosition = $(document).scrollTop(); //retrieve the document scroll ToP position
-                    var hScrollPosition = $(document).scrollLeft();
-                    console.log(vScrollPosition);
-                    console.log(hScrollPosition);
+                        console.log(currentpos);
+                        console.log($(document).scrollLeft());
+                        console.log((currentpos.left-initialpos.left));
 
-                     window.scrollTo(100, 100);
 
-                    //$(document).scrollLeft(viewportWidth * ($('#target').offsetLeft-x)/$('#target').width());
-                    //$(document).scrollTop(viewportHeight * ($('#target').offsetTop-y)/$('#target').height());
+                        $('body, html').scrollLeft($(document).scrollLeft()-(currentpos.left-initialpos.left)*30);
+                        //$('map').scrollLeft($('map').scrollLeft()+(currentpos.left-initialpos.left)*30);
+                        //$('body, html').scrollTop($(document).scrollTop()-(this.currentPosition.top-this.lastPosition.top)/1.1)
+                        //this.lastPosition = this.currentPosition;
+
 
                 }
 
